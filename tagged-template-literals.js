afed55de30d30a90df14bba9f7b36e234a05456a
Brendan Morrell <brendanmorrell@gmail.com>
2018-10-28 13:58:12 -0700

    changed

--- tagged-template-literals.js
+++ tagged-template-literals.js
@@ -1,7 +1,23 @@
 const taggedTemplateFunction = (strings, ...values) => {
-  console.log(strings)
-  console.log(values)
-}
+  console.log(strings);
+  let isBrendan = false;
+  return strings.reduce((str, x, i) => {
+    if (!isBrendan) {
+      str += x;
+      if (values[i]) {
+        str += values[i];
+        if (values[i] === 'Brendan') {
+          isBrendan = true;
+          const additionalMessage = "'s worst nightmare. also, brendan is a jackass";
+          str += additionalMessage;
+        }
+      }
+    }
+    return str;
+  }, '');
+};
 
-const user = 'Brendan'
-taggedTemplateFunction`blue cats are ${user}'s favorite things in the world dont you agree`
+const user = 'Brendan';
+console.log(
+  taggedTemplateFunction`blue cats are ${user}'s favorite things in the world dont you agree`
+);
